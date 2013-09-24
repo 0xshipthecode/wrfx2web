5106acb374121f2f478f80d03f9a56a545c753ce
Martin Vejmelka <vejmelkam@gmail.com>
2013-09-24 16:49:48 -0600

    converted to daemon backends started as -detached with console.sh script to access node

---
+++ console.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+erl -sname wrfx2web_cons -remsh wrfx2web@gross
--- startnode.sh
+++ startnode.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-erl -sname wrfx2web -pa ebin deps/*/ebin ../wrfx2/ebin/ -s wrfx2web
+erl -detached -sname wrfx2web -pa ebin deps/*/ebin ../wrfx2/ebin/ -s wrfx2web
 
